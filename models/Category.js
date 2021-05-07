899d36d6c0e91972c583dca4e8a6d6100d8b40cb
ABojo <alex.bojorquez2598@gmail.com>
2021-05-07 17:46:07 -0400

    Changed layout template to use the properties of the objects passed in now that Im not popuating it with dummy data + updated the virtual URL property on the Category Model

--- models/Category.js
+++ models/Category.js
@@ -5,7 +5,7 @@ const categorySchema = new mongoose.Schema({
 });
 
 categorySchema.virtual('url').get(function () {
-  return `/${this.name}`;
+  return `/category/${this.name}`;
 });
 
 const categoryModel = mongoose.model('Category', categorySchema);
