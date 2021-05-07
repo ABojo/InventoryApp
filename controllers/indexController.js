5302d8dfe9b13524622ade1b7b616fec81fc6ac8
ABojo <alex.bojorquez2598@gmail.com>
2021-05-07 18:27:25 -0400

    Setup create category template, route, and controller function

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,19 @@
 const Category = require('../models/Category');
 
-exports.getHomePage = async (req, res, next) => {
-  const categories = await Category.find();
-  res.render('index', { title: 'All Products', categoryList: categories });
+exports.getHomePage = async (req, res) => {
+  const categoryList = await Category.find();
+  res.render('index', { title: 'All Products', categoryList });
+};
+
+exports.getAddProductPage = (req, res) => {};
+
+exports.getAddCategoryPage = async (req, res) => {
+  const categoryList = await Category.find();
+  res.render('categoryForm', { categoryList });
+};
+
+exports.addCategory = async (req, res) => {
+  const { name, description } = req.body;
+  const newCategory = await Category.create({ name, description });
+  res.redirect(newCategory.url);
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const indexController = require('../controllers/indexController');
 
 router.get('/', indexController.getHomePage);
+router.get('/add-product', indexController.getAddProductPage);
+router.get('/add-category', indexController.getAddCategoryPage);
+router.post('/add-category', indexController.addCategory);
 
 module.exports = router;
